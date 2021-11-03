dae904505fe97386d70f4672f9ccff1230bbded6
SowjanyaGunaganti <89555872+SowjanyaGunaganti@users.noreply.github.com>
2021-11-03 18:35:05 -0500

    updated

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', grapesRouter);
-app.use('/users', addmodsRouter);
-app.use('/users', selectorRouter);
+app.use('/grapes', grapesRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
